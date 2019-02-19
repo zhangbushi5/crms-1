6727624f698cfe715ee075f589d7f0d0e16a229c
zhangbushi5 <841643923@qq.com>
2019-02-19 10:35:09 +0800

    test

---
+++ src/main/java/com/meibiao/crms/CrmsApplication.java
@@ -0,0 +1,13 @@
+package com.meibiao.crms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmsApplication.class, args);
+	}
+
+}
